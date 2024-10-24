5066e3e3f2294418744baf280336241cd6331579
ElizabethLuna1 <eli.liza.moon@gmail.com>
2024-10-24 17:04:41 -0600

    Se hacen cambios en CaluladoraImpl.java

--- src/main/java/calculadora/CaluladoraImpl.java
+++ src/main/java/calculadora/CaluladoraImpl.java
@@ -4,13 +4,13 @@ public class CaluladoraImpl implements Calculadora {
 
 	@Override
 	public int calcular(int i1, char op, int i2) {
-		switch (op) {
-		case '-':
-			return i1 - i2;
-		case '+':
-   		return i1 + i2;
-		default:
-			return 0;
+		switch(op){
+			case '+':
+				return i1+i2;
+			case '-':
+				return i1-i2;
+			default:
+				return 0;
 		}
 	}
 
